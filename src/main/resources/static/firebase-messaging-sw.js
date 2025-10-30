e717362ff00523dfd2438f1dcce723d30b2fb622
Mahmoud-belayeb <mahmoud.belayeb@tek-up.de>
2025-10-30 17:15:32 +0100

    Remove Firebase config from service worker
    Removed sensitive Firebase configuration details.

--- src/main/resources/static/firebase-messaging-sw.js
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -5,13 +5,7 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-comp
 
 // ⚠️ REMPLACEZ PAR VOTRE CONFIGURATION
 const firebaseConfig = {
-    apiKey: "AIzaSyA0-rt-JzOFy5Gqa-6-RV50ZXxIl62aPEI",
-    authDomain: "notification-push-test-tekup.firebaseapp.com",
-    projectId: "notification-push-test-tekup",
-    storageBucket: "notification-push-test-tekup.firebasestorage.app",
-    messagingSenderId: "112186616489",
-    appId: "1:112186616489:web:7547d31c6761ec7ab957b8",
-    measurementId: "G-2MF8TXBF32"
+
 };
 
 firebase.initializeApp(firebaseConfig);
